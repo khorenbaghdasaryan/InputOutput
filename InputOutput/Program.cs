49be1e18ba8badb990464431b6412a369bdbdf19
Khoren Baghdasaryan <khoren.a.baghdasaryan@gmail.com>
2020-05-03 21:11:37 +0400

    2 Commit

--- InputOutput/Program.cs
+++ InputOutput/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 
@@ -39,7 +41,7 @@ namespace InputOutput
         }
     }
 
-    public class JSONSerializers
+    public class JSON
     {
         public void Start()
         {
@@ -55,21 +57,70 @@ namespace InputOutput
                 new XElement("State", "Arabkir"),
                 new XElement("Country", "Armenia")));
             xDocument.Add(xElement);
-            Console.WriteLine(xDocument.ToString());
+            //Console.WriteLine(xDocument.ToString());
             xDocument.Save("sss.xml");
             XDocument xDocument1 = XDocument.Load("sss.xml");
-            Console.WriteLine(xDocument1);
+            //Console.WriteLine(xDocument1);
             string jsonString = JsonConvert.SerializeXNode(xDocument1);
             Console.WriteLine(jsonString);
             xDocument1 = JsonConvert.DeserializeXNode(jsonString);
-            Console.WriteLine(xDocument1);
+            //Console.WriteLine(xDocument1);
         }
+    }
+
+
+    [DataContract]
+    public class JSONSerializers
+    {
+        [DataContract]
+        public class Person
+        {
+            [DataMember]
+            public string FirstName { get; set; }
+            [DataMember]
+            public string LastName { get; set; }
+            [DataMember]
+            public int Age { get; set; }
+
+            public Person(string First, string Last, int Age)
+            {
+                FirstName = First;
+                LastName = Last;
+                this.Age = Age;
+            }
+        }
+       public void Start()
+       {
+            Person person1 = new Person("Ani", "Hovakimyan", 25);
+            Person person2 = new Person("Anna", "Baghdasaryan", 27);
+            Person person3 = new Person("Armine", "Baghdasaryan", 25);
+            Person[] persons = new Person[] { person1, person2, person3 };
+
+            DataContractJsonSerializer jsonSerializer =
+                new DataContractJsonSerializer(typeof(Person[]));
+
+            using (FileStream stream = new FileStream("person.json",
+                FileMode.OpenOrCreate))
+            {
+                jsonSerializer.WriteObject(stream, persons);
+            }
+
+            using (FileStream fileStream = new FileStream("person.json",
+                FileMode.OpenOrCreate))
+            {
+                Person[] peoples = (Person[])jsonSerializer.ReadObject(fileStream);
+                foreach (Person person in peoples)
+                {
+                    Console.WriteLine($"Name: {person.FirstName} Last: {person.LastName} Age: {person.Age}");
+                }
+            }
+
+       }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            new XmlSerializers().Start();
             new JSONSerializers().Start();
         }
     }
